05e1848f12276449caa69a9cfdc80e83e8a523a7
Karthik Raja <k.raja@accenture.com>
2012-10-29 15:51:17 +0530

    first commit

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -4,16 +4,7 @@ function init() {
     $("#search").click(search);
 	//$(".image-button").click(expand);
     gadgets.window.adjustHeight();
-	var gallery=new virtualpaginate({
-	piececlass: "firstdiv", //class of container for each piece of content
-	piececontainer: 'div', //container element type (ie: "div", "p" etc)
-	pieces_per_page: 1, //Pieces of content to show per page (1=1 piece, 2=2 pieces etc)
-	defaultpage: 0, //Default page selected (0=1st page, 1=2nd page etc). Persistence if enabled overrides this setting.
-	wraparound: false,
-	persist: false //Remember last viewed page and recall it when user returns within a browser session?
-})
-
-gallery.buildpagination(["paginate", "jspaginate"])
+	
    
 }
 $("span.image-button").live('click', function () {
