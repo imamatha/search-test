7a2359a73b94647c936b212e2ef2d78837ca8f95
Karthik Raja <k.raja@accenture.com>
2012-11-02 16:41:55 +0530

    first commit

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -576,18 +576,7 @@ function search() {
 			$("#tabs-3").html(document);
 			$("#tabs-4").html(post);
             $("#search-info").show();
-			var pager = new Imtech.Pager();
-			if(discussion !="")
-			{
-				$(document).ready(function() {
-					pager.paragraphsPerPage = 5; // set amount elements per page
-					pager.pagingContainer = $("#tabs-2").html(discussion); // set of main container
-					pager.paragraphs = $('div.firstdiv', pager.pagingContainer); // set of required containers
-					pager.showPage(1);
-					
-				});
-			}
-            gadgets.window.adjustHeight();
+			gadgets.window.adjustHeight();
         }
     });
 }
