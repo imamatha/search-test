1bcb399cdfbe2ed4158b352a00f6a3a99a5fc573
Mamatha Munivelu <munivelu.mamatha@accenture.com>
2012-11-09 14:04:13 +0530

    yes

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -396,7 +396,7 @@ function search() {
 			var loop_check_discussion=0;
 			var loop_check_document=0;
 			var loop_check_blog=0;
-			var items_per_page =1;
+			var items_per_page =3;
 			var newcontent = '';
 			var page_index=0;
 			var page="";
